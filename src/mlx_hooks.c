ae406193192de950ec82fbae998bed3d79c274bd
Clement Paluszek <cpalusze@z4r10p1.42lyon.fr>
2023-03-02 10:45:00 +0100

    Bug: hooks ping pong

--- src/mlx_hooks.c
+++ src/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:21:37 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/03/01 14:38:18 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/03/02 10:18:40 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ static int	key_release_hook(int key, t_cub3d *cube);
 
 void	set_mlx_hooks(t_cub3d *cube)
 {
-	mlx_hook(cube->mlx_data.mlx_win, ON_KEYDOWN, 0x1, key_press_hook, cube);
-	mlx_hook(cube->mlx_data.mlx_win, ON_KEYUP, 0x2, key_release_hook, cube);
+	mlx_hook(cube->mlx_data.mlx_win, ON_KEYDOWN, 1L << 0, key_press_hook, cube);
+	mlx_hook(cube->mlx_data.mlx_win, ON_KEYUP, 1L << 1, key_release_hook, cube);
 	mlx_hook(cube->mlx_data.mlx_win, ON_BUTTON_PRESS, 1L << 0, button_hook, cube);
 }
 
